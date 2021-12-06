635e4b66bdc4cf3f992e9219ddb92ac7a6b95e57
ralfgraef <graef.ralf@gmail.com>
2021-12-06 21:20:31 +0100

    changes_until_12_6

--- app/app.py
+++ app/app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key = True)
+    name = db.Column(db.String(length = 30), nullable = False, unique = True)
+    price = db.Column(db.Integer(), nullable = False)
+    barcode = db.Column(db.String(length = 12), nullable = False, unique = True)
+    description = db.Column(db.String(length = 1024), nullable = False, unique = True)
 
 app.config["UPLOAD_PATH"] = "/app/app/uploads"
 
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///market.db'
 
 @app.route("/")
 @app.route("/home")
@@ -14,11 +25,7 @@ def index():
 
 @app.route("/market")
 def market_page():
-    items = [
-    {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-    {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-    {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Item.query.all()
     return render_template('market.html', items=items)
 
 @app.route("/upload", methods = ['GET', 'POST'])
